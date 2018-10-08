79a048bdd457dcaf26ed937f7d786547d2379a06
Darshan Patel <42681834+darshanpatel44@users.noreply.github.com>
2018-10-08 20:48:36 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class hello
+{
+  public static void main(String args[])
+  {
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter your name");
+    String t=in.nextLine();
+    System.out.println("Hello "+t);
+  }
+}
